b4087816fd472976fdcef4fe8d741a728878b0f9
Alex Sandro <alex@creapix.com.br>
2024-07-01 11:35:17 -0300

    Entidaade atletas criação de models e schemas

---
+++ workout-api/atleta/schemas.py
@@ -0,0 +1,11 @@
+from typing import Annotated
+from pydantic import BaseModel
+
+class Atleta:
+    nome: Annotated[str, Field(description='Nome do atleta', example='Joao', max-length=50)]
+    cpf: Annotated[str, Field(description='Cpf do atleta', example='12345678900', max-length=11)]
+    idade: Annotated[int, Field(description='Idade do atleta', example='25')]
+    peso: Annotated[PositiveFloat, Field(description='Peso do atleta', example='75.0')]
+    altura: Annotated[PositiveFloat, Field(description='Altura do atleta', example='1.83')]
+    sexo: Annotated[str, Field(description='Sexo do atleta', example='M', max-length=1)]
+
