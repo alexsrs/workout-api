b9ef9f588ed38f22142bcb55cc6149a559f2e98b
Alex Sandro <alex@creapix.com.br>
2024-07-01 17:51:22 -0300

    restruturando tudo

--- workout-api/contrib/models.py
+++ workout-api/contrib/models.py
@@ -6,5 +6,3 @@ from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 
 class BaseModel(DeclarativeBase):
     id: Mapped[UUID] = mapped_column(PG_UUID(as_uuid=True), default=uuid4, nullable=False)
-
-    
