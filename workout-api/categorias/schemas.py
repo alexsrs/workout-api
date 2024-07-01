03ab95544784bbe6fdf9bd8007a807ded6150e30
Alex Sandro <alex@creapix.com.br>
2024-07-01 15:45:03 -0300

    usando dock composer e configuração do alembic

--- workout-api/categorias/schemas.py
+++ workout-api/categorias/schemas.py
@@ -1,6 +1,12 @@
 from typing import Annotated
-from contrib.schemas import BaseSchema
-from pydantic import Field
 
-class Categoria(BaseSchema):
-    nome: Annotated[str, Field(description='Nome do categoria', example='Scale', max-length=10)]
+from pydantic import UUID4, Field
+from contrib.schemas import BaseSchema
+
+
+class CategoriaIn(BaseSchema):
+    nome: Annotated[str, Field(description='Nome da categoria', example='Scale', max_length=10)]
+
+
+class CategoriaOut(CategoriaIn):
+    id: Annotated[UUID4, Field(description='Identificador da categoria')]
--- workout-api/centro_treinamento/schemas.py
+++ workout-api/centro_treinamento/schemas.py
@@ -1,8 +1,18 @@
 from typing import Annotated
-from contrib.schemas import BaseSchema
-from pydantic import Field
 
-class CentroTreinamento(BaseSchema):
-    nome: Annotated[str, Field(description='Nome do Centro de treinamento', example='CT Magrelo', max-length=20)]
-    endereco: Annotated[str, Field(description='Endereço do Centro de treinamento', example='Rua do arroz - 34', max-length=60)]
-    proprietario: Annotated[str, Field(description='Nome do Proprietario do Centro de treinamento', example='CT Magrelo', max-length=30)]
+from pydantic import Field, UUID4
+from contrib.schemas import BaseSchema
+
+
+class CentroTreinamentoIn(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT King', max_length=20)]
+    endereco: Annotated[str, Field(description='Endereço do centro de treinamento', example='Rua X, Q02', max_length=60)]
+    proprietario: Annotated[str, Field(description='Proprietario do centro de treinamento', example='Marcos', max_length=30)]
+
+
+class CentroTreinamentoAtleta(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT King', max_length=20)]
+
+
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[UUID4, Field(description='Identificador do centro de treinamento')]    
